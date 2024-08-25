d49dff258e8a99626fdaf8e64dd432676a6fe2ff
Aniketh Jana <109498641+anikethjana1@users.noreply.github.com>
2024-08-25 18:08:30 +0530

    Update admin.py

--- WebStreamer/bot/plugins/admin.py
+++ WebStreamer/bot/plugins/admin.py
@@ -1,6 +1,4 @@
 # This file is a part of FileStreamBot
-from telegram import Update
-from telegram.ext import ContextTypes
 import os
 import time
 import string
@@ -153,8 +151,3 @@ async def sts(c: Client, m: Message):
         await m.reply_text(text)
     else:
         await m.reply_text("Please Reply to a File")
-
-@StreamBot.on_message(filters.command("restart") & filters.private & filters.user(Var.OWNER_ID))
-async def restart(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Bot is restarting. See you soon!')
-    raise SystemExit()
